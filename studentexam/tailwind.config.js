c14611f6463b13c9fe9756d546acba679db5517c
Mhizzpwresh <111092990+Mhizzpwresh@users.noreply.github.com>
2022-08-16 12:12:12 +0100

    Revert "Revert " Student section""
    This reverts commit a45a8f1fdbb0f7166ba0d2155692a554e1f7b982.

---
+++ studentexam/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      width:{
+        '11/13': '84.615385' ,
+      }
+    },
+  },
+  plugins: [],
+}
